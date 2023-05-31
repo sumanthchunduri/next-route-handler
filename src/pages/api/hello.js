5c490a8fde1ab11234de870bcbffa3e4725fef14
Sumanth <sumanthchunduri123@gmail.com>
2023-06-01 00:01:47 +0530

    start

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+let db = [];
+
+export default async function handler(req, res) {
+  
+  if(req.method === 'GET') {
+    res.status(200).json(db)
+  }
+  else if(req.method === 'POST') {
+    const data = req.body;
+    db.push(JSON.parse(data));
+    // db.push(data);
+    res.status(201).json(db);
+  }
+
+  // res.status(200).json({ name: 'John Doe' })
 }
